825a490c75902fdfdae6daa5d86849e1125f00ba
Akshay Pandey <akshaywebdev888@gmail.com>
2021-01-14 10:42:53 +0530

    Added with server

--- Client/public/js/dashboard.js
+++ Client/public/js/dashboard.js
@@ -13,6 +13,8 @@ var loaderscreen = document.querySelector('.loader');
 var errorDisplay = document.querySelector('.errorDisplay');
 var closeErrorDisplayBtn = document.querySelector('.errorDisplay .validationError h5');
 
+var btnTester = document.querySelector('.tester');
+
 
 // Variables to use
 var color;
@@ -35,6 +37,74 @@ var colorCount = {
     blue:0,
     orange:0,
 };
+var tester = {
+    Upper:{
+        1:'green',
+        2:'blue',
+        3:'white',
+        4:'red',
+        5:'yellow',
+        6:'blue',
+        7:'white',
+        8:'yellow',
+        9:'green',
+    },
+    Front:{
+        1:'green',
+        2:'orange',
+        3:'red',
+        4:'red',
+        5:'blue',
+        6:'green',
+        7:'blue',
+        8:'yellow',
+        9:'red',
+    },
+    Right:{
+        1:'white',
+        2:'yellow',
+        3:'blue',
+        4:'orange',
+        5:'red',
+        6:'blue',
+        7:'white',
+        8:'white',
+        9:'orange',
+    },
+    Back:{
+        1:'orange',
+        2:'orange',
+        3:'orange',
+        4:'white',
+        5:'green',
+        6:'red',
+        7:'yellow',
+        8:'orange',
+        9:'green',
+    },
+    Left:{
+        1:'yellow',
+        2:'yellow',
+        3:'orange',
+        4:'blue',
+        5:'orange',
+        6:'green',
+        7:'red',
+        8:'green',
+        9:'red',
+    },
+    Lower:{
+        1:'blue',
+        2:'white',
+        3:'yellow',
+        4:'red',
+        5:'white',
+        6:'white',
+        7:'blue',
+        8:'green',
+        9:'yellow',
+    }
+}
  
 // Events 
 for(var i =1;i<=6;i++){
@@ -63,6 +133,8 @@ for(var i =0;i<=5;i++){
 closebtn.addEventListener('click',closemodal);          //Event to close modal
 
 finalBtn.addEventListener('click',finalColors);
+
+btnTester.addEventListener('click',testerfill);
 
 // EventListerner
 function selectColor(e){
@@ -245,17 +317,16 @@ function result(){
 
         // Stringify data object into json 
         const body = JSON.stringify(data);
-        
-        // start loading 
+
+        // loding Screen 
         loaderscreen.style.display = 'block';
-        dashboard.style.display = 'none'
+        dashboard.style.display = 'none';
 
         // Sending request to server api 
-        axios.post('https://localhost:8080/java-servlet-json-rubiks-cube/steps',body,config).then((res)=>{
-            loaderscreen.style.display = 'none';
-            location.href = 'result.js';
-        })
-
+        axios.post('http://localhost:8080/java-servlet-json-rubiks-cube/steps',body,config).then((res)=>{
+            localStorage.setItem('data',res.data);
+            location.href = '.result.html';
+        });
     } 
 }
 
@@ -268,3 +339,15 @@ function closeErrorDisplay(){
 var username = localStorage.getItem('username');
 greet.innerHTML = `Welcome ${username}`;
 
+
+
+// Creating testing button to fill the colors inside faces 
+
+function testerfill(){
+    for (var facename in tester) {
+        faceFilled[facename]=true;
+        for(var value in tester[facename]){
+            document.getElementById(`boxFillerDash${facename}${value}`).style.backgroundColor = tester[facename][value];
+        }
+    }
+ }
